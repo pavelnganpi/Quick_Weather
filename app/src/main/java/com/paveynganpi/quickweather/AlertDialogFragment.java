da670b9ee0d169202e4865ce219baeabaf92f5d0
paveytel <paveytel@buffalo.edu>
2015-01-21 17:37:46 -0500

    finished onCreateDialog()

--- app/src/main/java/com/paveynganpi/quickweather/AlertDialogFragment.java
+++ app/src/main/java/com/paveynganpi/quickweather/AlertDialogFragment.java
@@ -1,7 +1,25 @@
 package com.paveynganpi.quickweather;
 
+import android.app.AlertDialog;
+import android.app.Dialog;
+import android.app.DialogFragment;
+import android.content.Context;
+import android.os.Bundle;
+
 /**
  * Created by paveynganpi on 1/21/15.
  */
-public class AlertDialogFragment {
+public class AlertDialogFragment extends DialogFragment {
+
+    @Override
+    public Dialog onCreateDialog(Bundle savedInstanceState) {
+        Context context = getActivity();
+        AlertDialog.Builder builder = new AlertDialog.Builder(context);
+        builder.setTitle(context.getString(R.string.error_title))
+                .setMessage(context.getString(R.string.error_message))
+                .setPositiveButton(context.getString(R.string.error_ok_button_text), null);
+
+        Dialog dialog = builder.create();
+        return dialog;
+    }
 }
