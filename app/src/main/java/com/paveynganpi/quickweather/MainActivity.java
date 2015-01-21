b6d44259357d4cf29e078a90a6d8ac4cd530c723
paveytel <paveytel@buffalo.edu>
2015-01-21 17:22:24 -0500

    able to connect to forecast.io using okhttp

--- app/src/main/java/com/paveynganpi/quickweather/MainActivity.java
+++ app/src/main/java/com/paveynganpi/quickweather/MainActivity.java
@@ -2,38 +2,72 @@ package com.paveynganpi.quickweather;
 
 import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
 
+import com.squareup.okhttp.Call;
+import com.squareup.okhttp.Callback;
+import com.squareup.okhttp.OkHttpClient;
+import com.squareup.okhttp.Request;
+import com.squareup.okhttp.Response;
+
+import java.io.IOException;
+
 
 public class MainActivity extends ActionBarActivity {
+
+    private static final String TAG = MainActivity.class.getSimpleName();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        String apiKey = "6d41fa95e035d900060eefa2f45cab81";
+        double latitude = 37.8267;
+        double longitude = -122.423;
+
+        String forecastUrl = "https://api.forecast.io/forecast/" + apiKey+ "/"+ latitude + ","+ longitude;
+
+        OkHttpClient client = new OkHttpClient();
+        Request request = new Request.Builder()
+                .url(forecastUrl).
+                build();
+
+        Call call = client.newCall(request);
+        call.enqueue(new Callback() {
+            @Override
+            public void onFailure(Request request, IOException e) {
+                Log.v(TAG,"error from request");
+
+            }
+
+            @Override
+            public void onResponse(Response response) throws IOException {
+
+                try {
+                    Log.v(TAG, response.body().string());
+
+                    if (!response.isSuccessful()) {
+                        alertUserAboutError();
+                    } else {
+
+                    }
+                }
+                catch (IOException e){
+                    Log.e(TAG,"Exception caught :",e);
+                }
+            }
+        });
+
+    }
+
+    private void alertUserAboutError() {
+
+
+
     }
 
 
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.menu_main, menu);
-        return true;
-    }
-
-    @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
-        int id = item.getItemId();
-
-        //noinspection SimplifiableIfStatement
-        if (id == R.id.action_settings) {
-            return true;
-        }
-
-        return super.onOptionsItemSelected(item);
-    }
 }
